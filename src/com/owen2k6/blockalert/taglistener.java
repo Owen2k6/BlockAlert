15e4e9d79b25a224d70807144b4f2041b7b35237
Owen2k6 <fulptdm@gmail.com>
2023-02-03 19:03:41 +0000

    fixed some lost changes from the PR that was made.

--- src/com/owen2k6/blockalert/taglistener.java
+++ src/com/owen2k6/blockalert/taglistener.java
@@ -32,6 +32,7 @@ public class taglistener implements Listener {
 		List<String> tagblock = baConfig.getTaggedBlocks();
 		List<Integer> taggedBlockIDs = baConfig.getTaggedIDs();
 		if (tagblock == null) return;
+		if (event.getPlayer().hasPermission("blockalert.exempt")) return;
 		Block block = event.getBlock();
 		Location blockLoc = block.getLocation();
 		String blockLocSerial = String.format("%s:%s:%s", blockLoc.getBlockX(), blockLoc.getBlockY(), blockLoc.getBlockZ());
@@ -46,31 +47,28 @@ public class taglistener implements Listener {
 			}
 			for (Player player : Bukkit.getOnlinePlayers())
 				if (player.hasPermission("blockalert.alert"))
-					player.sendMessage(ChatColor.RED +"BlockAlert: " + event.getPlayer().getName() + " has broken a " + event.getBlock().getType().toString() + " at " + event.getBlock().getX()+" "+event.getBlock().getY()+" "+event.getBlock().getZ() + " in world " + event.getBlock().getWorld().getName());
-			plugin.log.info("BlockAlert: " + event.getPlayer().getName() + " has broken a " + event.getBlock().getType().toString() + " at " + event.getBlock().getX()+" "+event.getBlock().getY()+" "+event.getBlock().getZ() + " in world " + event.getBlock().getWorld().getName());
+					player.sendMessage(ChatColor.RED + "BlockAlert: " + event.getPlayer().getName() + " has broken a " + event.getBlock().getType().toString() + " at " + event.getBlock().getX() + " " + event.getBlock().getY() + " " + event.getBlock().getZ() + " in world " + event.getBlock().getWorld().getName());
+			plugin.log.info("BlockAlert: " + event.getPlayer().getName() + " has broken a " + event.getBlock().getType().toString() + " at " + event.getBlock().getX() + " " + event.getBlock().getY() + " " + event.getBlock().getZ() + " in world " + event.getBlock().getWorld().getName());
 		}
 	}
 
 	@EventHandler
-	public void onPistonExtend(BlockPistonExtendEvent event)
-	{
+	public void onPistonExtend(BlockPistonExtendEvent event) {
 		List<String> tags = baConfig.getTaggedBlocks();
 		boolean pushingTaggedBlocks = false;
 
-		for (Block block : event.getBlocks())
-		{
+		for (Block block : event.getBlocks()) {
 			if (pushingTaggedBlocks) break;
-			if (tags.contains(getTagLocation(block.getLocation())))
-			{
+			if (tags.contains(getTagLocation(block.getLocation()))) {
 				pushingTaggedBlocks = true;
 				event.setCancelled(true);
 				break;
 			}
 		}
 
-		if (pushingTaggedBlocks)
-		{
-			if (isDiscordEnabled) sendDiscordMsg("BlockAlert: Piston @ " + getFriendlyLocation(event.getBlock().getLocation()) + " has been prevented from pushing tagged blocks.");
+		if (pushingTaggedBlocks) {
+			if (isDiscordEnabled)
+				sendDiscordMsg("BlockAlert: Piston @ " + getFriendlyLocation(event.getBlock().getLocation()) + " has been prevented from pushing tagged blocks.");
 			for (Player player : Bukkit.getOnlinePlayers())
 				if (player.hasPermission("blockalert.alert"))
 					player.sendMessage(ChatColor.RED + "BlockAlert: Piston @ " + getFriendlyLocation(event.getBlock().getLocation()) + " has been prevented from pushing tagged blocks.");
@@ -79,10 +77,8 @@ public class taglistener implements Listener {
 	}
 
 	@EventHandler
-	public void onPlayerInteract(PlayerInteractEvent event)
-	{
-		if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getPlayer().getItemInHand().getType() == Material.ARROW && event.getPlayer().hasPermission("blockalert.tag"))
-		{
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getPlayer().getItemInHand().getType() == Material.ARROW && event.getPlayer().hasPermission("blockalert.tag")) {
 			Block block = event.getClickedBlock();
 			if (block == null) return;
 			Location blockLoc = block.getLocation();
@@ -96,23 +92,20 @@ public class taglistener implements Listener {
 			baConfig.save();
 			//This is unessasary since this is not a required thing for staff to know. Moving the log to the console seems more practical.
 			//if (isDiscordEnabled) sendDiscordMsg("BlockAlert: " + event.getPlayer().getName() + " has " + (tagblock.contains(blockLocSerial) ? "tagged" : "untagged") + " a " + block.getType().toString() + " @ " + getFriendlyLocation(blockLoc));
-			//event.getPlayer().sendMessage(ChatColor.GREEN + "BlockAlert: " + (tagblock.contains(blockLocSerial) ? "Tagged" : "Untagged") + " block at " + getFriendlyLocation(blockLoc));
+			event.getPlayer().sendMessage(ChatColor.GREEN + "BlockAlert: " + (tagblock.contains(blockLocSerial) ? "Tagged" : "Untagged") + " block at " + getFriendlyLocation(blockLoc));
 			plugin.log.info("BlockAlert: " + event.getPlayer().getName() + " has " + (tagblock.contains(blockLocSerial) ? "tagged" : "untagged") + " a " + block.getType().toString() + " @ " + getFriendlyLocation(blockLoc));
 		}
 	}
 
-	private void sendDiscordMsg(String msg)
-	{
+	private void sendDiscordMsg(String msg) {
 		discordCore.getDiscordBot().discordSendToChannel(discordChannelID, msg);
 	}
 
-	private String getFriendlyLocation(Location loc)
-	{
+	private String getFriendlyLocation(Location loc) {
 		return String.format("%s %s %s", loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
 	}
 
-	private String getTagLocation(Location loc)
-	{
+	private String getTagLocation(Location loc) {
 		return String.format("%s:%s:%s", loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
 	}
 }
